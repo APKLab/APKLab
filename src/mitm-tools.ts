ec8f79e230d38af7226579711de736eebaea5c4c
Aman Sharma <amsharma44@outlook.com>
2020-12-13 00:44:06 +0530

    Disable certificate pinning

--- src/mitm-tools.ts
+++ src/mitm-tools.ts
@@ -2,6 +2,8 @@ import * as xml from 'xml-js';
 import * as fs from 'fs';
 import { promisify } from 'util';
 import * as path from 'path';
+import * as globby from 'globby';
+import * as escapeStringRegexp from 'escape-string-regexp';
 
 const DEFAULT_CONFIG = `<?xml version="1.0" encoding="utf-8"?>
 <network-security-config>
@@ -11,6 +13,38 @@ const DEFAULT_CONFIG = `<?xml version="1.0" encoding="utf-8"?>
         </trust-anchors>
     </debug-overrides>
 </network-security-config>`;
+
+const INTERFACE_LINE = '.implements Ljavax/net/ssl/X509TrustManager;';
+
+/** The methods that need to be patched to disable certificate pinning. */
+const METHOD_SIGNATURES = [
+    'checkClientTrusted([Ljava/security/cert/X509Certificate;Ljava/lang/String;)V',
+    'checkServerTrusted([Ljava/security/cert/X509Certificate;Ljava/lang/String;)V',
+    'getAcceptedIssuers()[Ljava/security/cert/X509Certificate;',
+];
+
+/** Patterns used to find the methods defined in `METHOD_SIGNATURES`. */
+const METHOD_PATTERNS = METHOD_SIGNATURES.map(signature => {
+    const escapedSignature = escapeStringRegexp(signature);
+    return new RegExp(
+        `(\\.method public (?:final )?${escapedSignature})\\n([^]+?)\\n(\\.end method)`,
+        'g',
+    );
+});
+
+/** Code inserted into `checkClientTrusted` and `checkServerTrusted`. */
+const RETURN_VOID_FIX = [
+    '.locals 0',
+    'return-void',
+];
+
+/** Code inserted into `getAcceptedIssuers`. */
+const RETURN_EMPTY_ARRAY_FIX = [
+    '.locals 1',
+    'const/4 v0, 0x0',
+    'new-array v0, v0, [Ljava/security/cert/X509Certificate;',
+    'return-object v0',
+];
 
 async function getNscFromManifest(manifestPath: string) {
     const manifestContent = await fs.promises.readFile(manifestPath, { encoding: 'utf-8' });
@@ -62,7 +96,7 @@ async function modifyNetworkSecurityConfig(nscPath: string) {
     }
     catch (err) {
         console.log(err);
-        if(err.includes('ENOENT')) {
+        if (err.includes('ENOENT')) {
             // File does not exist, create a default one
             await fs.promises.mkdir(path.dirname(nscPath), { recursive: true });
             await fs.promises.writeFile(nscPath, DEFAULT_CONFIG);
@@ -70,7 +104,61 @@ async function modifyNetworkSecurityConfig(nscPath: string) {
     }
 }
 
+async function disableCertificatePinning(directoryPath: string) {
 
+    try {
+
+        const smaliFiles = await globby(path.posix.join(directoryPath, 'smali*/**/*.smali'));
+
+        let pinningFound = false;
+
+        for (const filePath of smaliFiles) {
+            // observer.next(`Scanning ${path.basename(filePath)}...`)
+
+            const originalContent = await fs.promises.readFile(filePath, 'utf-8');
+
+            // Don't scan classes that don't implement the interface
+            if (!originalContent.includes(INTERFACE_LINE)) {
+                continue;
+            }
+
+            let patchedContent = originalContent;
+
+            for (const pattern of METHOD_PATTERNS) {
+                patchedContent = patchedContent.replace(pattern, (_, openingLine: string, body: string, closingLine: string) => {
+
+                    const bodyLines = body.split('\n').map(line => line.replace(/^    /, ''));
+
+                    const fixLines = openingLine.includes('getAcceptedIssuers') ? RETURN_EMPTY_ARRAY_FIX : RETURN_VOID_FIX;
+
+                    const patchedBodyLines = [
+                        '# inserted by APKLab to disable certificate pinning',
+                        ...fixLines,
+                        '',
+                        '# commented out by APKLab to disable old method body',
+                        '# ',
+                        ...bodyLines.map(line => `# ${line}`)
+                    ];
+
+                    return [openingLine, ...patchedBodyLines.map(line => `    ${line}`), closingLine,].map(line => line.replace(/\s+$/, '')).join('\n');
+                });
+            }
+
+            if (originalContent !== patchedContent) {
+                pinningFound = true;
+                await fs.promises.writeFile(filePath, patchedContent);
+            }
+
+        }
+
+        if (!pinningFound) {
+            console.log('No certificate pinning logic found.');
+        }
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
 
 export namespace mitmTools {
 
@@ -88,6 +176,7 @@ export namespace mitmTools {
         console.log(nscName);
         console.log(nscPath);
         await modifyNetworkSecurityConfig(nscPath);
+        await disableCertificatePinning(decodeDir);
     }
 
 }
