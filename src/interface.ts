9cf1f1ed314c9fb566a6a744d1ff90bb0ed4df07
Aman Sharma <amsharma44@users.noreply.github.com>
2020-12-20 20:35:26 +0530

    Add apk-mitm patch (#40)
    Co-authored-by: Surendrajat <surendrajat@protonmail.com>

--- src/interface.ts
+++ src/interface.ts
@@ -1,71 +1,11 @@
 import { QuickPickItem, window } from 'vscode';
 import { apktool } from './tools';
 import { outputChannel } from './common';
+import { mitmTools } from './mitm-tools';
+import { quickPickUtil } from './quick-pick.util';
 
-/**
- * QuickPickItem array for additional **Apktool** args for APK decoding.
- */
-const decodeQuickPickItems: QuickPickItem[] = [
-    {
-        label: "decompile_java",
-        detail: "Decompiles APK to Java source using Jadx",
-        description: "[Use Jadx]"
-    },
-    {
-        label: "--no-src",
-        detail: "Do not decompile dex to smali (-s)",
-        alwaysShow: true
-    },
-    {
-        label: "--no-res",
-        detail: "Do not decompile resources (-r)",
-        alwaysShow: true
-    },
-    {
-        label: "--force-manifest",
-        detail: "Forces decode of AndroidManifest regardless of decoding of resources flag."
-    },
-    {
-        label: "--no-assets",
-        detail: "Prevents decoding/copying of unknown asset files.",
-        alwaysShow: true
-    },
-    {
-        label: "--only-main-classes",
-        detail: "Only disassemble dex classes in root (classes[0-9]*.dex)"
-    },
-    {
-        label: "--no-debug-info",
-        detail: "Prevents baksmali from writing out debug info (.local, .param, .line, etc). (-b)"
-    }
-];
 
-/**
- * QuickPickItem array for additional **Apktool** args for APK building.
- */
-const rebuildQuickPickItems: QuickPickItem[] = [
-    {
-        label: "--use-aapt2",
-        detail: "Use the aapt2 binary instead of appt",
-        alwaysShow: true,
-        picked: true
-    },
-    {
-        label: "--debug",
-        detail: "Build APK in Debug mode by adding debuggable=\"true\" to AndroidManifest file",
-        alwaysShow: true,
-    },
-    {
-        label: "--force-all",
-        detail: "Overwrites existing files during build, reassembling the resources.arsc file and dex file(s)",
-        alwaysShow: true
-    },
-    {
-        label: "--no-crunch",
-        detail: "Disable crunching of resource files during the build step",
-        alwaysShow: true
-    }
-];
+
 
 
 export namespace UI {
@@ -100,7 +40,7 @@ export namespace UI {
             openLabel: "Select an APK file",
         });
         if (result && result.length === 1) {
-            let args = await showArgsQuickPick(decodeQuickPickItems, 'Additional apktool/jadx arguments');
+            let args = await showArgsQuickPick(quickPickUtil.getQuickPickItems('decodeQuickPickItems'), 'Additional apktool/jadx arguments');
             if (args) {
                 const decompileJavaIndex = args.indexOf("decompile_java");
                 let decompileJava = false;
@@ -120,9 +60,16 @@ export namespace UI {
      * @param apktoolYmlPath path of the `apktool.yml` file.
      */
     export async function rebuildAPK(apktoolYmlPath: string) {
-        const args = await showArgsQuickPick(rebuildQuickPickItems, 'Additional apktool arguments');
+        const args = await showArgsQuickPick(quickPickUtil.getQuickPickItems('rebuildQuickPickItems'), 'Additional apktool arguments');
         if (args) {
             apktool.rebuildAPK(apktoolYmlPath, args);
         }
+    }
+
+    /**
+     * @param apktoolYmlPath path of the `apktool.yml` file.
+     */
+    export async function applyMitmPatch(apktoolYmlPath: string) {
+        mitmTools.applyMitmPatch(apktoolYmlPath);
     }
 }
