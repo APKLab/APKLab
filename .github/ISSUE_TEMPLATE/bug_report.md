0b3c544a297053e0f35530264e53966dde1e6ae4
Surendrajat <surendrajat@protonmail.com>
2020-12-27 22:52:21 +0530

    adapt .editorconfig & .gitattributes (#54)

--- .github/ISSUE_TEMPLATE/bug_report.md
+++ .github/ISSUE_TEMPLATE/bug_report.md
@@ -1,10 +1,9 @@
 ---
 name: Bug report
 about: Create a report to help us improve
-title: ''
-labels: ''
-assignees: ''
-
+title: ""
+labels: ""
+assignees: ""
 ---
 
 **Describe the bug**
@@ -12,6 +11,7 @@ A clear and concise description of what the bug is.
 
 **To Reproduce**
 Steps to reproduce the behavior:
+
 1. Go to '...'
 2. Click on '....'
 3. See error
@@ -20,6 +20,7 @@ Steps to reproduce the behavior:
 A clear and concise description of what you expected to happen.
 
 **Console Logs**
+
 <details>
 
 Please paste all your console output here... between the details HTML tags!
@@ -27,8 +28,9 @@ Please paste all your console output here... between the details HTML tags!
 </details>
 
 **System Information:**
- - OS: [e.g. Window 10 or Ububntu 20.04]
- - VSCode Version: [e.g. 1.45.1-preview]
+
+- OS: [e.g. Window 10 or Ububntu 20.04]
+- VSCode Version: [e.g. 1.45.1-preview]
 
 **Additional context**
 Add any other context about the problem here.
--- src/extension.ts
+++ src/extension.ts
@@ -7,52 +7,52 @@ import { UI } from './interface';
 
 export function activate(context: vscode.ExtensionContext) {
 
-	console.log('Activated apklab extension!');
+    console.log('Activated apklab extension!');
 
-	// command for opening an apk file for decoding
-	const openApkFileCommand = vscode.commands.registerCommand('apklab.openApkFile', async () => {
-		updateTools().then(async () => {
-			UI.decompileAPK();
-		}).catch(() => {
-			outputChannel.appendLine("Can't download/update dependencies!");
-		});
-	});
+    // command for opening an apk file for decoding
+    const openApkFileCommand = vscode.commands.registerCommand('apklab.openApkFile', async () => {
+        updateTools().then(async () => {
+            UI.decompileAPK();
+        }).catch(() => {
+            outputChannel.appendLine("Can't download/update dependencies!");
+        });
+    });
 
-	// command for rebuilding apk file
-	const rebuildAPkFileCommand = vscode.commands.registerCommand("apklab.rebuildApkFile", (uri: vscode.Uri) => {
-		updateTools().then(() => {
-			UI.rebuildAPK(uri.fsPath);
-		}).catch(() => {
-			outputChannel.appendLine("Can't download/update dependencies!");
-		});
-	});
+    // command for rebuilding apk file
+    const rebuildAPkFileCommand = vscode.commands.registerCommand("apklab.rebuildApkFile", (uri: vscode.Uri) => {
+        updateTools().then(() => {
+            UI.rebuildAPK(uri.fsPath);
+        }).catch(() => {
+            outputChannel.appendLine("Can't download/update dependencies!");
+        });
+    });
 
-	// command for installing apk file
-	const installAPkFileCommand = vscode.commands.registerCommand("apklab.installApkFile", (uri: vscode.Uri) => {
-		adb.installAPK(uri.fsPath);
-	});
+    // command for installing apk file
+    const installAPkFileCommand = vscode.commands.registerCommand("apklab.installApkFile", (uri: vscode.Uri) => {
+        adb.installAPK(uri.fsPath);
+    });
 
-	// command for patching files for https inspection
-	const patchApkForHttpsCommand = vscode.commands.registerCommand("apklab.patchApkForHttps", (uri: vscode.Uri) => {
-		updateTools().then(() => {
-			UI.applyMitmPatch(uri.fsPath);
-		}).catch(() => {
-			outputChannel.appendLine("Can't download/update dependencies!");
-		});
-	});
+    // command for patching files for https inspection
+    const patchApkForHttpsCommand = vscode.commands.registerCommand("apklab.patchApkForHttps", (uri: vscode.Uri) => {
+        updateTools().then(() => {
+            UI.applyMitmPatch(uri.fsPath);
+        }).catch(() => {
+            outputChannel.appendLine("Can't download/update dependencies!");
+        });
+    });
 
-	// command to empty apktool framework resource dir
-	const emptyFrameworkDirCommand = vscode.commands.registerCommand("apklab.emptyFrameworkDir", () => {
-		updateTools().then(() => {
-			apktool.emptyFrameworkDir();
-		}).catch(() => {
-			outputChannel.appendLine("Can't download/update dependencies!");
-		});
-	});
+    // command to empty apktool framework resource dir
+    const emptyFrameworkDirCommand = vscode.commands.registerCommand("apklab.emptyFrameworkDir", () => {
+        updateTools().then(() => {
+            apktool.emptyFrameworkDir();
+        }).catch(() => {
+            outputChannel.appendLine("Can't download/update dependencies!");
+        });
+    });
 
-	context.subscriptions.push(
-		openApkFileCommand, rebuildAPkFileCommand, installAPkFileCommand, patchApkForHttpsCommand, emptyFrameworkDirCommand
-	);
+    context.subscriptions.push(
+        openApkFileCommand, rebuildAPkFileCommand, installAPkFileCommand, patchApkForHttpsCommand, emptyFrameworkDirCommand
+    );
 }
 
 export function deactivate() { }
