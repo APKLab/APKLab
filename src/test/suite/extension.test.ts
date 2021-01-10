5a122a588b627f04f7de86d9029af0cdd5250925
Surendrajat <surendrajat@protonmail.com>
2021-01-10 15:16:48 +0530

    test: install needed tools

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,15 +1,27 @@
 import * as assert from "assert";
+import { test } from "mocha";
+import { updateTools } from "../../downloader";
+import { apktool } from "../../tools";
 
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
-import * as vscode from "vscode";
-// import * as myExtension from '../../extension';
+suite("Extension Test Suite", function () {
+    this.timeout(600000);
+    console.log("Start all tests...");
 
-suite("Extension Test Suite", () => {
-    vscode.window.showInformationMessage("Start all tests.");
+    suiteSetup(async () => {
+        console.log("Installing the tools...");
+        await updateTools()
+            .then(() => {
+                console.log("Tools Installed!");
+            })
+            .catch(() => {
+                console.log("Failed to install tools!");
+                assert.fail("Failed to install tools!");
+            });
+    });
 
     test("Sample test", () => {
         assert.strictEqual(-1, [1, 2, 3].indexOf(5));
         assert.strictEqual(-1, [1, 2, 3].indexOf(0));
-    });
+        apktool.decodeAPK("test.apk", [], true);
+    }).timeout(1200000);
 });
