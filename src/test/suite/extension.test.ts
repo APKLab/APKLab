410ddfebc3eed356b504314ba86a749560031c90
Surendrajat <surendrajat@protonmail.com>
2021-01-10 21:47:18 +0530

    test: add apktool decode test

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -3,10 +3,14 @@ import { test } from "mocha";
 import * as path from "path";
 import { updateTools } from "../../downloader";
 import { apktool } from "../../tools";
+import * as fs from "fs";
 
 suite("Extension Test Suite", function () {
     this.timeout(600000);
     console.log("Start all tests...");
+
+    const testDataDir = path.resolve(__dirname, "../../../testdata");
+    const simpleKeyboardDir = path.join(testDataDir, "simplekeyboard");
 
     suiteSetup(async () => {
         console.log("Installing the tools...");
@@ -20,11 +24,19 @@ suite("Extension Test Suite", function () {
             });
     });
 
-    test("Sample test", function () {
-        this.timeout(1000000);
-        console.log("here");
-        const testApkPath = path.resolve(__dirname, "../../../test.apk");
-        apktool.decodeAPK(testApkPath, [], true);
-        console.log("reached here");
+    test("Decode SimpleKeyboard.apk", async function () {
+        const testApkPath = path.resolve(simpleKeyboardDir, "test.apk");
+        console.log(`Decoding ${testApkPath}...`);
+        await apktool.decodeAPK(testApkPath, [], false);
+        const jsonFilePath = path.join(simpleKeyboardDir, "decoded_files.json");
+        const jsonFileData = fs.readFileSync(jsonFilePath, "utf-8");
+        const decodedFiles: string[] = JSON.parse(jsonFileData);
+        console.log("Comparing file list...");
+        decodedFiles.forEach((file) => {
+            if (!fs.existsSync(path.join(simpleKeyboardDir, "test", file))) {
+                assert.fail(`File ${file} not found!`);
+            }
+        });
+    });
     });
 });
