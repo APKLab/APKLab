c2bb4fc381ea5d776643cab1ce9604320144a2cd
Surendrajat <surendrajat@protonmail.com>
2021-01-10 18:01:15 +0530

    lint: more perttier and eslint changes (#60)
    * fmt: use prettier on all files
    
    * ignore .vscode-test
    
    * lint: decouple eslint & prettier errors
    
    * lint: pretty check during linting
    
    * fmt/lint: auto-fix code formatting and eslint errors
    
    * lint: disable @typescript-eslint/no-explicit-any for now

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,26 @@
 //@ts-check
 
-'use strict';
+"use strict";
 
-const path = require('path');
+const path = require("path");
 
 /**@type {import('webpack').Configuration}*/
 const config = {
-    target: 'node',
-    entry: './src/extension.ts', // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
+    target: "node",
+    entry: "./src/extension.ts", // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'extension.js',
-        libraryTarget: 'commonjs2',
-        devtoolModuleFilenameTemplate: '../[resource-path]'
+        path: path.resolve(__dirname, "dist"),
+        filename: "extension.js",
+        libraryTarget: "commonjs2",
+        devtoolModuleFilenameTemplate: "../[resource-path]",
     },
-    devtool: 'source-map',
+    devtool: "source-map",
     externals: {
-        vscode: 'commonjs vscode' // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
+        vscode: "commonjs vscode", // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
     },
     resolve: {
         // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
-        extensions: ['.ts', '.js']
+        extensions: [".ts", ".js"],
     },
     module: {
         rules: [
@@ -29,11 +29,11 @@ const config = {
                 exclude: /node_modules/,
                 use: [
                     {
-                        loader: 'ts-loader'
-                    }
-                ]
-            }
-        ]
-    }
+                        loader: "ts-loader",
+                    },
+                ],
+            },
+        ],
+    },
 };
 module.exports = config;
