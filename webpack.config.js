c41c958b3b81380fbaecb25596781b5900d8eddf
Surendrajat <surendrajat@protonmail.com>
2021-02-19 17:26:36 +0530

    dep: update webpack[-cli] and ts-loader (#119)
    * update webpack[-cli] and ts-loader
    
    * update webpack config
    
    * update deps

--- webpack.config.js
+++ webpack.config.js
@@ -8,20 +8,24 @@ const webpack = require("webpack");
 /**@type {import('webpack').Configuration}*/
 const config = {
     target: "node",
+    mode: "none",
+
     entry: "./src/extension.ts", // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "extension.js",
         libraryTarget: "commonjs2",
-        devtoolModuleFilenameTemplate: "../[resource-path]",
     },
-    devtool: "source-map",
+    devtool: "nosources-source-map",
     externals: {
         vscode: "commonjs vscode", // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
     },
     resolve: {
         // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
         extensions: [".ts", ".js"],
+    },
+    infrastructureLogging: {
+        level: "log",
     },
     module: {
         rules: [
