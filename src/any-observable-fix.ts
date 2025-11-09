bfe0933d05b204dae583a0bcc1cb419056cec79e
Surendrajat <surendrajat@protonmail.com>
2025-11-09 16:56:04 +0530

    webpack -> esbuild (#163)
    * webpack -> esbuild
    
    * esbuild: remove APK_MITM_VERSION webpack plugin
    
    * ci: update node version

--- src/any-observable-fix.ts
+++
@@ -1,6 +0,0 @@
-// This file is necessary because webpack will fail to load the `any-observable`
-// package (which is a transitive dependency of `apk-mitm`) otherwise. This file
-// replaces that module and hard-codes the `Observable` implementation from
-// `rxjs` as the one that is used by packages which import `any-observable`.
-
-export { Observable as default } from "rxjs";
