9f512ccc0fc226b570ccae325aa86c1c0081b26f
surendrajat <surendrajat@protonmail.com>
2025-11-09 21:03:53 +0530

    lint fixes

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -1,45 +1,45 @@
 /* eslint-disable no-undef */
 import { context } from "esbuild";
 
-const production = process.argv.includes('--production');
-const watch = process.argv.includes('--watch');
+const production = process.argv.includes("--production");
+const watch = process.argv.includes("--watch");
 
 /**
  * @type {import('esbuild').Plugin}
  */
 const esbuildProblemMatcherPlugin = {
-    name: 'esbuild-problem-matcher',
+    name: "esbuild-problem-matcher",
 
     setup(build) {
         build.onStart(() => {
-            console.log('[watch] build started');
+            console.log("[watch] build started");
         });
         build.onEnd((result) => {
             result.errors.forEach(({ text, location }) => {
                 console.error(`✘ [ERROR] ${text}`);
-                console.error(`    ${location.file}:${location.line}:${location.column}:`);
+                console.error(
+                    `    ${location.file}:${location.line}:${location.column}:`,
+                );
             });
-            console.log('[watch] build finished');
+            console.log("[watch] build finished");
         });
     },
 };
 
 async function main() {
     const ctx = await context({
-        entryPoints: [
-            'src/extension.ts'
-        ],
+        entryPoints: ["src/extension.ts"],
         bundle: true,
-        format: 'cjs',
+        format: "cjs",
         minify: production,
         sourcemap: !production,
         sourcesContent: false,
-        platform: 'node',
-        outfile: 'dist/extension.js',
-        external: ['vscode'],
-        logLevel: 'silent',
+        platform: "node",
+        outfile: "dist/extension.js",
+        external: ["vscode"],
+        logLevel: "silent",
         alias: {
-            'any-observable': './src/any-observable-fix.ts'
+            "any-observable": "./src/any-observable-fix.ts",
         },
         plugins: [
             /* add to the end of plugins array */
@@ -54,7 +54,7 @@ async function main() {
     }
 }
 
-main().catch(e => {
+main().catch((e) => {
     console.error(e);
     process.exit(1);
 });
